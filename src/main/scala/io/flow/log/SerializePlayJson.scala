05b455d2713c5ed1935fe34cc5ae434b8d410515
Ben Iofel <ben@flow.io>
2018-08-14 11:38:18 -0400

    let logstash serialize Play JSON

---
+++ src/main/scala/io/flow/log/SerializePlayJson.scala
@@ -0,0 +1,10 @@
+import com.fasterxml.jackson.databind.MappingJsonFactory
+import net.logstash.logback.decorate.JsonFactoryDecorator
+import play.api.libs.json.jackson.PlayJsonModule
+
+class SerializePlayJson extends JsonFactoryDecorator {
+  override def decorate(factory: MappingJsonFactory): MappingJsonFactory = {
+    factory.getCodec.registerModule(PlayJsonModule)
+    factory
+  }
+}

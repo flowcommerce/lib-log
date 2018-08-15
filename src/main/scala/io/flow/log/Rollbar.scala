eb10f0dc79d44cc7a71e2730ebd31b08f2a99170
Ben Iofel <iofelben+github@gmail.com>
2018-08-15 10:43:14 -0400

    structured logging (#26)
    Untested, let's try it in payment by using `"io.flow" %% "lib-log" % "0.0.25-sl"`
    
    you can use [this](https://github.com/flowcommerce/content/blob/master/api/conf/logback.xml) logback.xml

--- src/main/scala/io/flow/log/Rollbar.scala
+++ src/main/scala/io/flow/log/Rollbar.scala
@@ -1,20 +1,25 @@
 package io.flow.log
 
-import com.google.gson.{GsonBuilder, JsonParser}
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.{ObjectMapper, SerializerProvider}
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.google.inject.assistedinject.{Assisted, AssistedInject, FactoryModuleBuilder}
 import com.google.inject.{AbstractModule, Provider}
+import com.rollbar.api.payload.Payload
 import com.rollbar.api.payload.data.Data
 import com.rollbar.notifier.Rollbar
 import com.rollbar.notifier.config.ConfigBuilder
 import com.rollbar.notifier.fingerprint.FingerprintGenerator
+import com.rollbar.notifier.sender.result.Result
+import com.rollbar.notifier.sender.{BufferedSender, SyncSender}
 import io.flow.util.{Config, FlowEnvironment}
 import javax.inject.{Inject, Singleton}
 import net.codingwell.scalaguice.ScalaModule
 import org.slf4j.LoggerFactory
-import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.libs.json.jackson.PlayJsonModule
+import play.api.libs.json._
 
 import scala.collection.JavaConverters._
-import scala.util.Try
 
 class RollbarModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
@@ -38,7 +43,49 @@ class RollbarProvider @Inject() (
 
     val fingerprintGenerator = new FingerprintGenerator {
       override def from(data: Data): String = {
-        Try(data.getCustom.get(RollbarLogger.Keys.Fingerprint)).toOption.map(_.toString).orNull
+        Option(data.getCustom)
+          .flatMap(custom => Option(custom.get(RollbarLogger.Keys.Fingerprint)))
+          .map(_.toString)
+          .orNull
+      }
+    }
+
+    // give a jackson serializer to rollbar, instead of using rollbar's hand-rolled serializer
+    val jacksonSerializer = new com.rollbar.notifier.sender.json.JsonSerializer {
+      // plain old jackson serializer
+      val mapper = new ObjectMapper()
+
+      // de/serialize play-json types
+      mapper.registerModule(PlayJsonModule)
+
+      // serialize Rollbar JsonSerializable types
+      mapper.registerModule(new SimpleModule() {
+        addSerializer(
+          classOf[com.rollbar.api.json.JsonSerializable],
+          (value: com.rollbar.api.json.JsonSerializable, gen: JsonGenerator, serializers: SerializerProvider) => {
+            serializers.defaultSerializeValue(value.asJson(), gen)
+          }
+        )
+      })
+
+      override def toJson(payload: Payload): String = {
+        mapper.writeValueAsString(payload)
+      }
+
+      case class ResultObj(code: Int = -1, message: Option[String], uuid: Option[String])
+
+      override def resultFrom(response: String): Result = {
+        Json.parse(response).validate(Json.reads[ResultObj]) match {
+          case JsSuccess(obj, _) =>
+            val builder = new Result.Builder().code(obj.code)
+            if (obj.code == 0)
+              builder.body(obj.uuid.get)
+            else
+              builder.body(obj.message.get)
+            builder.build()
+          case _ =>
+            new Result.Builder().code(-1).body("Didn't get an object").build()
+        }
       }
     }
 
@@ -46,6 +93,16 @@ class RollbarProvider @Inject() (
       .handleUncaughtErrors(true)
       .language("scala")
       .fingerPrintGenerator(fingerprintGenerator)
+      .sender(
+        new BufferedSender.Builder()
+          .sender(
+            new SyncSender.Builder()
+              .accessToken(token)
+              .jsonSerializer(jacksonSerializer)
+              .build()
+          )
+          .build()
+      )
       .environment(FlowEnvironment.Current.toString)
       .build()
   }
@@ -94,13 +151,9 @@ object RollbarLogger {
     val OrderNumber = "order_number"
     val Fingerprint = "fingerprint"
   }
-  val gson = new GsonBuilder().disableHtmlEscaping().create()
-  val jsonParser = new JsonParser()
 
-  // TODO: See if there's a less expensive way to perform this conversion
-  def convert(attributes: Map[String, JsValue]): java.util.Map[String, Object] = (attributes map { case (key, value) =>
-    key -> gson.toJson(jsonParser.parse(Json.toJson(value).toString)).asInstanceOf[Object]
-  }).asJava
+  def convert(attributes: Map[String, JsValue]): java.util.Map[String, Object] =
+    attributes.asJava.asInstanceOf[java.util.Map[String, Object]]
 }
 
 case class RollbarLogger @AssistedInject() (
@@ -110,6 +163,7 @@ case class RollbarLogger @AssistedInject() (
   config: Config
 ) {
   import RollbarLogger._
+  import net.logstash.logback.marker.Markers.appendEntries
 
   private val logger = LoggerFactory.getLogger("application")
 
@@ -127,17 +181,17 @@ case class RollbarLogger @AssistedInject() (
   def error(message: => String): Unit = error(message, null)
 
   def warn(message: => String, error: => Throwable): Unit = {
-    logger.warn(legacyMessage.getOrElse(message), error)
+    logger.warn(appendEntries(convert(attributes)), legacyMessage.getOrElse(message), error)
     rollbar.foreach(_.warning(error, convert(attributes), message))
   }
 
   def info(message: => String): Unit = {
-    logger.info(legacyMessage.getOrElse(message))
+    logger.info(appendEntries(convert(attributes)), legacyMessage.getOrElse(message))
     //not sending to rollbar to save quota
   }
 
   def error(message: => String, error: => Throwable): Unit = {
-    logger.error(legacyMessage.getOrElse(message))
+    logger.error(appendEntries(convert(attributes)), legacyMessage.getOrElse(message))
     rollbar.foreach(_.error(error, convert(attributes), message))
   }
 }
---
+++ src/main/scala/io/flow/log/SerializePlayJson.scala
@@ -0,0 +1,13 @@
+package io.flow.log
+
+import com.fasterxml.jackson.databind.MappingJsonFactory
+import net.logstash.logback.decorate.JsonFactoryDecorator
+import play.api.libs.json.jackson.PlayJsonModule
+
+// allows you to pass play-json types to logstash-logback-encoder
+class SerializePlayJson extends JsonFactoryDecorator {
+  override def decorate(factory: MappingJsonFactory): MappingJsonFactory = {
+    factory.getCodec.registerModule(PlayJsonModule)
+    factory
+  }
+}

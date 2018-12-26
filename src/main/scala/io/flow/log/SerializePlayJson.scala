6849f3555ff2b21ae3a331dde6c8d6e3bbf97286
Matthew Mark Miller <das@dasmegabyte.org>
2018-12-26 11:48:26 -0500

    Upgrade libraries: com.rollbar, com.typesafe.play (#58)
    * Upgrade libraries: com.rollbar, com.typesafe.play
    
    * Commenting out tpolecat to disable deprecation elevation until latest play-json code is consistent with advice
    
    * Commenting out tpolecat to disable deprecation elevation until latest play-json code is consistent with advice
    
    * Re-enabling tpolecat; adding silencer plugin
    
    * TODO

--- src/main/scala/io/flow/log/SerializePlayJson.scala
+++ src/main/scala/io/flow/log/SerializePlayJson.scala
@@ -1,13 +1,14 @@
 package io.flow.log
 
 import com.fasterxml.jackson.databind.MappingJsonFactory
+import com.github.ghik.silencer.silent
 import net.logstash.logback.decorate.JsonFactoryDecorator
 import play.api.libs.json.jackson.PlayJsonModule
 
 // allows you to pass play-json types to logstash-logback-encoder
 class SerializePlayJson extends JsonFactoryDecorator {
   override def decorate(factory: MappingJsonFactory): MappingJsonFactory = {
-    factory.getCodec.registerModule(PlayJsonModule)
+    factory.getCodec.registerModule(PlayJsonModule): @silent //TODO: please remove once PlayJsonModule(jsonParsersettings) is accessible outside of the jackson package
     factory
   }
 }

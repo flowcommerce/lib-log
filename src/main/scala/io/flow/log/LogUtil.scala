02fc3b702e11d904d37b681dafab54682759363f
Jean Logeart <jean.logeart@gmail.com>
2019-11-20 16:26:40 -0500

    Allow duration to be sampled (#126)
    * Allow duration to be sampled
    
    * Ipdate
    
    * Fix

--- src/main/scala/io/flow/log/LogUtil.scala
+++ src/main/scala/io/flow/log/LogUtil.scala
@@ -2,10 +2,16 @@ package io.flow.log
 
 import javax.inject.Inject
 
+import scala.util.Random
+
 class LogUtil @Inject() (logger: RollbarLogger) {
 
   import RollbarLogger.Keys
 
+  /**
+    * @param frequency Log once per frequency. For instance, 100 means that the duration will be logged once every 100
+    *                  calls on average.
+    */
   def duration[T](
     info: String,
     fingerprint: String,
@@ -14,25 +20,28 @@ class LogUtil @Inject() (logger: RollbarLogger) {
     experienceKey: Option[String] = None,
     orderNumber: Option[String] = None,
     requestId: Option[String] = None,
-    data: Option[Map[String, String]] = None
+    data: Option[Map[String, String]] = None,
+    frequency: Long = 1L,
   )(f: => T): T = {
-    val start = System.currentTimeMillis()
-    try {
+    if (frequency == 1L || Random.nextLong() % frequency == 0) {
+      val start = System.currentTimeMillis()
+      try {
+        f
+      } finally {
+        val end = System.currentTimeMillis()
+
+        logger
+          .fingerprint(fingerprint)
+          .organization(organizationId)
+          .withKeyValue(Keys.RequestId, requestId)
+          .withKeyValue(Keys.ExperienceKey, experienceKey)
+          .withKeyValue(Keys.ItemNumber, itemNumber)
+          .withKeyValue(Keys.OrderNumber, orderNumber)
+          .withKeyValue("data", data)
+          .withKeyValue("duration", end - start)
+          .info(info)
+      }
+    } else
       f
-    } finally {
-      val end = System.currentTimeMillis()
-
-      logger
-        .fingerprint(fingerprint)
-        .organization(organizationId)
-        .withKeyValue(Keys.RequestId, requestId)
-        .withKeyValue(Keys.ExperienceKey, experienceKey)
-        .withKeyValue(Keys.ItemNumber, itemNumber)
-        .withKeyValue(Keys.OrderNumber, orderNumber)
-        .withKeyValue("data", data)
-        .withKeyValue("duration", end - start)
-        .info(info)
-    }
   }
-
 }

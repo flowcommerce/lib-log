0fc8cf53c88989c63796eeab70e21e9e4ef2839e
Darren McDonald <darrenmc@users.noreply.github.com>
2020-04-13 18:10:59 -0400

    Only execute future once when logging duration (#143)
    * Add scalatest dependency
    
    * Write failing test for log duration future executed twice
    
    * Only execute future once when logging duration

---
+++ src/test/scala/io/flow/log/LogUtilSpec.scala
@@ -0,0 +1,43 @@
+package io.flow.log
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class LogUtilSpec extends AnyWordSpec with Matchers {
+
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+  "LogUtil" when {
+    val logUtil = new LogUtil(RollbarLogger.SimpleLogger)
+
+    "log duration" should {
+      "only execute function once" in {
+        val callCount = new AtomicInteger(0)
+        def f = {
+          callCount.incrementAndGet()
+        }
+        logUtil.duration(info = "test", fingerprint = "test", organizationId = "test")(f)
+        callCount.get must be (1)
+      }
+    }
+
+    "log duration future" should {
+      "only execute future once" in {
+        val callCount = new AtomicInteger(0)
+        def f = Future {
+          callCount.incrementAndGet()
+        }
+        Await.result(
+          logUtil.durationF(info = "test", fingerprint = "test", organizationId = "test")(f),
+          10.millis
+        )
+        callCount.get must be (1)
+      }
+    }
+  }
+}
